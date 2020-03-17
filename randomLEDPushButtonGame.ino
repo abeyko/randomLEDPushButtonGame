801a556013bdd014634c76f1abc0da1dc62c6251
Angeliki Beyko <abeyko@users.noreply.github.com>
2020-03-17 18:30:15 -0500

    Update randomLEDPushButtonGame.ino

--- randomLEDPushButtonGame.ino
+++ randomLEDPushButtonGame.ino
@@ -1,84 +1,56 @@
-//Arduino program
-//Created  y Angeliki Beyko on 2014.12.20
+// Arduino program
+// Last modified by Angeliki Beyko on 2020.03.17
 
 const int maxcount = 4; 
 const int button[] = { 4,5,6,7};  
 const int led[] = { 9,10,11,12};  
-int lightedled = -1;  
-int speed = 1000; 
  
 unsigned long start, finished, elapsed; 
  
-void setup(){  
-  // Set all pins as output 
-  for(int i=0; i < maxcount; i++) {  
+void setup() {  
+  // Set all led pins as outputs 
+  for (int i = 0; i < maxcount; i++) {  
     pinMode(led[i], OUTPUT); 
   } 
-  // Set all buttons as input 
-  for(int i=0; i < maxcount; i++) { 
-    pinMode(button[i], INPUT); 
-    Serial.begin(9600); 
-  } 
+  
+  // Set all button pins as inputs 
+  for (int i = 0; i < maxcount; i++) { 
+    pinMode(button[i], INPUT);   
+  }
+
+  // Start the serial monitor to view output
+  Serial.begin(9600);
 }
 
- 
-//clears all the leds on the bus 
-void clearleds() 
-{ 
-  for(int f=0; f <maxcount; f++)  
-  { 
-    digitalWrite (led[f], LOW);
-  } 
-  lightedled = -1; 
-    delay(speed);
-} 
- 
-void lightrandom() 
-{ 
-  lightedled = random(maxcount); 
-  digitalWrite(led[lightedled], HIGH); 
-}
+void loop() {
+  
+  // generate a random number and store it in a variable
+  int randNumber = random(maxcount);
 
- 
-//returns the index of the first pressed button, blocks until a button is pressed. 
-int readbutton()  
-{ 
-    while(true) 
-    { 
-        for(int x = 0; x < maxcount; x++) 
-        { 
-            
-                        if(!digitalRead(button[x])) 
-            { 
-                return x; 
-                Serial.println(x) 
-            } 
-        } 
-    }
-}
+  // clear the leds
+  for (int i = 0; i < maxcount; i++) {
+    digitalWrite(led[i], LOW);
+  }
 
- 
-void loop(){ 
-  clearleds(); 
-  lightrandom(); 
-  start = millis(); 
-  int button = readbutton(); 
-  finished = millis();  
-  if(button == lightedled) 
-  { 
-      Serial.println("true"); 
-        elapsed = finished - start;  
-        Serial.print(elapsed); 
-        Serial.println(" milliseconds elapsed"); 
-        Serial.println(); 
-  } 
-  if(button!=lightedled)
-  {
-    Serial.println("Try again!");
-    elapsed = finished - start;
-    Serial.println(elapsed + " This is how long it took you in ms");
-    Serial.println();
-  } 
-  delay(speed); 
-}
+  // start the timer
+  start = millis();
 
+  // enter while loop
+  while (digitalRead(button[randNumber]) == LOW) {
+    // random led will stay on until button is pressed
+    digitalWrite(led[randNumber], HIGH);
+  }
+
+  // exit while loop, correct button pressed
+  // record end time
+  finished = millis();
+
+  // store how many ms it took for user to press correct button
+  elapsed = finished - start;
+
+  // Print out how many ms ellapsed
+  if (elapsed > 100) {
+    Serial.print(elapsed);
+    Serial.println(" ms have passed");
+  }
+}
